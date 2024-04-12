8fc3bb6804dc1958e4da72111603c8abc1e9c6ee
John Jacko <jljacko@outlook.com>
2024-04-12 14:15:57 -0500

    Made fixes to config class, fix phone number class that enables it to be encrypted and later unencrypted

--- src/main/java/com/trecapps/auth/encryptors/EncryptorConfiguration.java
+++ src/main/java/com/trecapps/auth/encryptors/EncryptorConfiguration.java
@@ -1,5 +1,6 @@
 package com.trecapps.auth.encryptors;
 
+import com.azure.spring.cloud.autoconfigure.implementation.condition.ConditionalOnMissingProperty;
 import com.trecapps.auth.keyholders.AKVEncryptorKeyHolder;
 import com.trecapps.auth.keyholders.IEncryptorKeyHolder;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,8 +12,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class EncryptorConfiguration {
 
-    @ConditionalOnMissingBean(IFieldEncryptor.class)
     @Bean
+    @ConditionalOnMissingProperty(prefix = "trecauth.encryption", name = "strategy")
     public IFieldEncryptor getDefaultEncryptor(){
         return new BasicFieldEncryptor();
     }
@@ -29,12 +30,12 @@ public class EncryptorConfiguration {
     @Bean
     @ConditionalOnProperty(prefix = "trecauth.encryption", name="strategy", havingValue = "RSA")
     IFieldEncryptor getRsaEncryptor(
-            @Value("trecauth.keyvault.name") String vaultName,
-            @Value("trecauth.keyvault.tenantId") String tenantId,
-            @Value("trecauth.keyvault.clientId") String clientId,
-            @Value("trecauth.keyvault.clientSecret") String clientSecret,
-            @Value("trecauth.keyvault.rsa.public-name") String publicKey,
-            @Value("trecauth.keyvault.rsa.public-name") String privateKey
+            @Value("${trecauth.keyvault.name}") String vaultName,
+            @Value("${trecauth.keyvault.tenantId}") String tenantId,
+            @Value("${trecauth.keyvault.clientId}") String clientId,
+            @Value("${trecauth.keyvault.clientSecret}") String clientSecret,
+            @Value("${trecauth.keyvault.rsa.public-name}") String publicKey,
+            @Value("${trecauth.keyvault.rsa.public-name}") String privateKey
     ){
         IEncryptorKeyHolder keyHolder = getEncryptorKeyHolder(vaultName, tenantId, clientId, clientSecret);
         return new RsaFieldEncryptor(keyHolder, publicKey, privateKey);
@@ -43,13 +44,13 @@ public class EncryptorConfiguration {
     @Bean
     @ConditionalOnProperty(prefix = "trecauth.encryption", name="strategy", havingValue = "AES")
     IFieldEncryptor getAesEncryptor(
-            @Value("trecauth.keyvault.name") String vaultName,
-            @Value("trecauth.keyvault.tenantId") String tenantId,
-            @Value("trecauth.keyvault.clientId") String clientId,
-            @Value("trecauth.keyvault.clientSecret") String clientSecret,
-            @Value("trecauth.keyvault.aes.password") String password,
-            @Value("trecauth.keyvault.aes.salt") String salt,
-            @Value("trecauth.keyvault.aes.iv") String iv
+            @Value("${trecauth.keyvault.name}") String vaultName,
+            @Value("${trecauth.keyvault.tenantId}") String tenantId,
+            @Value("${trecauth.keyvault.clientId}") String clientId,
+            @Value("${trecauth.keyvault.clientSecret}") String clientSecret,
+            @Value("${trecauth.keyvault.aes.password}") String password,
+            @Value("${trecauth.keyvault.aes.salt}") String salt,
+            @Value("${trecauth.keyvault.aes.iv}") String iv
     ){
         IEncryptorKeyHolder keyHolder = getEncryptorKeyHolder(vaultName, tenantId, clientId, clientSecret);
         return new AesFieldEncryptor(keyHolder, password, salt, iv);
--- src/main/java/com/trecapps/auth/models/PhoneNumber.java
+++ src/main/java/com/trecapps/auth/models/PhoneNumber.java
@@ -3,6 +3,7 @@ package com.trecapps.auth.models;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.trecapps.auth.encryptors.EncryptedField;
+import com.trecapps.auth.encryptors.IFieldEncryptor;
 import lombok.Data;
 
 @Data
@@ -13,14 +14,17 @@ public class PhoneNumber {
     private PhoneCountryCode countryCode = PhoneCountryCode.US;
 
     @JsonCreator
-    public PhoneNumber(
-            @JsonProperty("number") long number){
-        setNumber(number);
+    public PhoneNumber(@JsonProperty("number") String number){
+        // If field is currently encrypted, then unrealistic to expect pure numbers to be included
+        if(number.startsWith(IFieldEncryptor.FRONT_WRAP) && number.endsWith(IFieldEncryptor.BACK_WRAP))
+            this.number = number;
+        else
+            // It is not encrypted, thus reasonable to expect String to be parsable as a long
+            this.number = String.valueOf(Long.parseLong(number));
     }
 
-    @JsonCreator
-    public PhoneNumber(@JsonProperty("number") String number){
-        this.number = String.valueOf(Long.parseLong(number));
+    public PhoneNumber(long number){
+        setNumber(number);
     }
 
     public void setNumber(long number){
