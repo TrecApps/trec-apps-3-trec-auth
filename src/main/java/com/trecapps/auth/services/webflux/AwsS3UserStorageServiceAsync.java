32601c581cb50f6ddd14ebebce0b7a777f05cec0
John Jacko <jljacko@outlook.com>
2024-05-13 20:59:08 -0500

    Created Reactive Versions of the User Storage Service

---
+++ src/main/java/com/trecapps/auth/services/webflux/AwsS3UserStorageServiceAsync.java
@@ -0,0 +1,218 @@
+package com.trecapps.auth.services.webflux;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.trecapps.auth.encryptors.IFieldEncryptor;
+import com.trecapps.auth.models.*;
+import lombok.SneakyThrows;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import reactor.core.publisher.Mono;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.BytesWrapper;
+import software.amazon.awssdk.core.async.AsyncRequestBody;
+import software.amazon.awssdk.core.async.AsyncResponseTransformer;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class AwsS3UserStorageServiceAsync implements IUserStorageServiceAsync {
+
+
+    S3AsyncClient client;
+    ObjectMapper objectMapper;
+    String app;
+
+    String s3BucketName;
+
+    IFieldEncryptor encryptor;
+
+    AwsS3UserStorageServiceAsync(String clientName,
+                            String clientSecret,
+                            String s3Region,
+                            String s3BucketName,
+                            String app,
+                            IFieldEncryptor encryptor1,
+                            Jackson2ObjectMapperBuilder objectMapperBuilder){
+        AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(clientName, clientSecret);
+        StaticCredentialsProvider staticCredentialsProvider = StaticCredentialsProvider.create(awsBasicCredentials);
+                client = S3AsyncClient.builder()
+                        .credentialsProvider(staticCredentialsProvider)
+                        .region(Region.of(s3Region)).build();
+
+        setUp(s3BucketName, app, encryptor1, objectMapperBuilder);
+    }
+
+    AwsS3UserStorageServiceAsync(
+                            String s3Region,
+                            String s3BucketName,
+                            String app,
+                            IFieldEncryptor encryptor1,
+                            Jackson2ObjectMapperBuilder objectMapperBuilder){
+        client = S3AsyncClient.builder()
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .region(Region.of(s3Region)).build();
+
+        setUp(s3BucketName, app, encryptor1, objectMapperBuilder);
+    }
+
+    void setUp(String s3BucketName,
+               String app,
+               IFieldEncryptor encryptor1,
+               Jackson2ObjectMapperBuilder objectMapperBuilder)
+    {
+        objectMapper = objectMapperBuilder.createXmlMapper(false).build();
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        this.app = app;
+        this.s3BucketName = s3BucketName;
+        this.encryptor = encryptor1;
+    }
+
+    @SneakyThrows(IOException.class)
+    @Override
+    public Mono<String> retrieveKey(String keyId) {
+        GetObjectRequest request = GetObjectRequest.builder()
+                .bucket(this.s3BucketName)
+                .key(keyId)
+                .build();
+
+        return Mono.fromFuture(client.getObject(request, AsyncResponseTransformer.toBytes()))
+                .map(BytesWrapper::asUtf8String);
+    }
+
+
+    @SneakyThrows
+    <T> T getContent(String content, Class<T> type){
+        return objectMapper.readValue(content, type);
+    }
+
+    @SneakyThrows
+    @Override
+    public Mono<Optional<TcUser>> getAccountById(String id) {
+        GetObjectRequest request = GetObjectRequest.builder()
+                .bucket(this.s3BucketName)
+                .key("user-" + id)
+                .build();
+
+        return Mono.fromFuture(client.getObject(request, AsyncResponseTransformer.toBytes()))
+                .map(BytesWrapper::asUtf8String)
+                .map((String content) ->getContent(content, TcUser.class))
+                .map((TcUser user) -> encryptor.decrypt(user))
+                .map(Optional::of)
+                .onErrorReturn(Optional.empty());
+    }
+
+    @SneakyThrows(IOException.class)
+    @Override
+    public Mono<Optional<SessionList>> retrieveSessions(String id) throws JsonProcessingException {
+        GetObjectRequest request = GetObjectRequest.builder()
+                .bucket(this.s3BucketName)
+                .key("sessions-" + id)
+                .build();
+
+        return Mono.fromFuture(client.getObject(request, AsyncResponseTransformer.toBytes()))
+                .map(BytesWrapper::asUtf8String)
+                .map((String content) ->getContent(content, SessionList.class))
+                .map((SessionList user) -> encryptor.decrypt(user))
+                .map(Optional::of)
+                .onErrorReturn(Optional.empty());
+    }
+
+    @SneakyThrows
+    @Override
+    public Mono<Optional<TcBrands>> getBrandById(String id) {
+
+        GetObjectRequest request = GetObjectRequest.builder()
+                .bucket(this.s3BucketName)
+                .key("brand-" + id)
+                .build();
+
+        return Mono.fromFuture(client.getObject(request, AsyncResponseTransformer.toBytes()))
+                .map(BytesWrapper::asUtf8String)
+                .map((String content) ->getContent(content, TcBrands.class))
+                .map((TcBrands user) -> encryptor.decrypt(user))
+                .map(Optional::of)
+                .onErrorReturn(Optional.empty());
+    }
+
+
+    @SneakyThrows(IOException.class)
+    @Override
+    public Mono<Optional<AppLocker>> retrieveAppLocker(String id) throws JsonProcessingException {
+        GetObjectRequest request = GetObjectRequest.builder()
+                .bucket(this.s3BucketName)
+                .key("logins-" + id + ".json")
+                .build();
+
+        return Mono.fromFuture(client.getObject(request, AsyncResponseTransformer.toBytes()))
+                .map(BytesWrapper::asUtf8String)
+                .map((String content) ->getContent(content, AppLocker.class))
+                .map((AppLocker user) -> encryptor.decrypt(user))
+                .map(Optional::of)
+                .onErrorResume(e -> Mono.fromCallable(() -> {
+                    AppLocker ret = new AppLocker();
+                    Map<String, FailedLoginList> list = new HashMap<>();
+                    FailedLoginList logins = new FailedLoginList();
+                    logins.setFailedLogins(new ArrayList<>());
+                    list.put(app, logins);
+                    ret.setLoginListMap(list);
+                    return Optional.of(ret);
+                }));
+
+    }
+
+    @SneakyThrows
+    @Override
+    public void saveLogins(AppLocker locker, String id) {
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(this.s3BucketName)
+                .key("logins-" + id + ".json")
+                .build();
+
+        client.putObject(putObjectRequest,
+                AsyncRequestBody.fromString(objectMapper.writeValueAsString(encryptor.encrypt(locker))));
+    }
+    @SneakyThrows
+    @Override
+    public void saveUser(TcUser user) {
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(this.s3BucketName)
+                .key("user-" + user.getId())
+                .build();
+
+        client.putObject(putObjectRequest,
+                AsyncRequestBody.fromString(objectMapper.writeValueAsString(encryptor.encrypt(user))));
+    }
+    @SneakyThrows
+    @Override
+    public void saveBrand(TcBrands brand) {
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(this.s3BucketName)
+                .key("brand-" + brand.getId())
+                .build();
+
+        client.putObject(putObjectRequest,
+                AsyncRequestBody.fromString(objectMapper.writeValueAsString(encryptor.encrypt(brand))));
+    }
+    @SneakyThrows
+    @Override
+    public void saveSessions(SessionList brand, String id) {
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(this.s3BucketName)
+                .key("sessions-" + id)
+                .build();
+
+        client.putObject(putObjectRequest,
+                AsyncRequestBody.fromString(objectMapper.writeValueAsString(encryptor.encrypt(brand))));
+    }
+}
---
+++ src/main/java/com/trecapps/auth/services/webflux/AzureBlobUserStorageServiceAsync.java
@@ -0,0 +1,172 @@
+package com.trecapps.auth.services.webflux;
+
+import com.azure.core.credential.AzureNamedKeyCredential;
+import com.azure.core.util.BinaryData;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.storage.blob.*;
+import com.azure.storage.blob.specialized.BlobAsyncClientBase;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.trecapps.auth.encryptors.IFieldEncryptor;
+import com.trecapps.auth.models.*;
+import com.trecapps.auth.services.web.IUserStorageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+
+public class AzureBlobUserStorageServiceAsync implements IUserStorageServiceAsync {
+
+    BlobServiceAsyncClient client;
+    BlobContainerAsyncClient containerClient;
+
+    ObjectMapper objectMapper;
+
+
+    String app;
+
+    IFieldEncryptor encryptor;
+
+
+    AzureBlobUserStorageServiceAsync(String name,
+                       String key,
+                       String endpoint,
+                       String containerName,
+                       String app,
+                       IFieldEncryptor encryptor1,
+                       Jackson2ObjectMapperBuilder objectMapperBuilder)
+    {
+        AzureNamedKeyCredential credential = new AzureNamedKeyCredential(name, key);
+        client = new BlobServiceClientBuilder().credential(credential).endpoint(endpoint).buildAsyncClient();
+        objectMapper = objectMapperBuilder.createXmlMapper(false).build();
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        this.app = app;
+        this.encryptor = encryptor1;
+        containerClient = client.getBlobContainerAsyncClient(containerName);
+    }
+
+    AzureBlobUserStorageServiceAsync(
+            String endpoint,
+            String containerName,
+            String app,
+            IFieldEncryptor encryptor1,
+            Jackson2ObjectMapperBuilder objectMapperBuilder
+    ){
+        client = new BlobServiceClientBuilder().credential(new DefaultAzureCredentialBuilder().build()).endpoint(endpoint).buildAsyncClient();
+        objectMapper = objectMapperBuilder.createXmlMapper(false).build();
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        this.app = app;
+        this.encryptor = encryptor1;
+        containerClient = client.getBlobContainerAsyncClient(containerName);
+    }
+
+    @Override
+    public Mono<String> retrieveKey(String keyId)
+    {
+        return Mono.just(keyId)
+                .map(k -> containerClient.getBlobAsyncClient(k))
+                .map(BlobAsyncClientBase::downloadContent)
+                .flatMap((Mono<BinaryData> bData)->
+                        bData.map((BinaryData bd) ->new String(bd.toBytes(), StandardCharsets.UTF_8)));
+    }
+
+    <T> Mono<Optional<T>> downloadContent(BlobAsyncClient client, Class<T> type){
+        return Mono.just(client)
+                .map(BlobAsyncClient::downloadContent)
+                .flatMap((Mono<BinaryData> bData)->
+                        bData.map((BinaryData bd) -> {
+                            T obj = bd.toObject(type);
+                            obj = encryptor.decrypt(obj);
+                            return Optional.of(obj);
+                        }));
+
+    }
+
+    @Override
+    public Mono<Optional<TcUser>> getAccountById(String id){
+        BlobAsyncClient client = containerClient.getBlobAsyncClient("user-" + id);
+        return Mono.just(client)
+                .map(BlobAsyncClientBase::exists)
+                .flatMap((Mono<Boolean> exists) ->
+                        exists.flatMap((Boolean e) -> !e ? Mono.just(Optional.empty()) : downloadContent(client, TcUser.class)));
+    }
+
+    @Override
+    public Mono<Optional<SessionList>> retrieveSessions(String id) throws JsonProcessingException {
+        BlobAsyncClient client = containerClient.getBlobAsyncClient("sessions-" + id);
+        return Mono.just(client)
+                .map(BlobAsyncClientBase::exists)
+                .flatMap((Mono<Boolean> exists) ->
+                        exists.flatMap((Boolean e) -> !e ? Mono.just(Optional.empty()) : downloadContent(client, SessionList.class)));
+    }
+
+    @Override
+    public Mono<Optional<TcBrands>> getBrandById(String id) {
+        BlobAsyncClient client = containerClient.getBlobAsyncClient("brand-" + id);
+        return Mono.just(client)
+                .map(BlobAsyncClientBase::exists)
+                .flatMap((Mono<Boolean> exists) ->
+                        exists.flatMap((Boolean e) -> !e ? Mono.just(Optional.empty()) : downloadContent(client, TcBrands.class)));
+    }
+
+    @Override
+    public Mono<Optional<AppLocker>> retrieveAppLocker(String id) throws JsonProcessingException
+    {
+        BlobAsyncClient client = containerClient.getBlobAsyncClient("brand-" + id);
+        return Mono.just(client)
+                .map(BlobAsyncClientBase::exists)
+                .flatMap((Mono<Boolean> exists) ->
+                        exists.flatMap((Boolean e) ->
+                        {
+                            if(e)
+                                return downloadContent(client, AppLocker.class);
+                            AppLocker ret = new AppLocker();
+                            Map<String, FailedLoginList> list = new HashMap<>();
+                            FailedLoginList logins = new FailedLoginList();
+                            logins.setFailedLogins(new ArrayList<>());
+                            list.put(app, logins);
+                            ret.setLoginListMap(list);
+                            return Mono.just(Optional.of(ret));
+                        }));
+    }
+
+    @Override
+    public void saveLogins(AppLocker locker, String id)
+    {
+        BlobAsyncClient client = containerClient.getBlobAsyncClient("logins-" + id + ".json");
+
+        client.upload(BinaryData.fromObject(encryptor.encrypt(locker)),true).subscribe();
+    }
+
+    @Override
+    public void saveUser(TcUser user)
+    {
+        BlobAsyncClient client = containerClient.getBlobAsyncClient("user-" + user.getId());
+
+        client.upload(BinaryData.fromObject(encryptor.encrypt(user)),true).subscribe();
+    }
+
+    @Override
+    public void saveBrand(TcBrands brand)
+    {
+        BlobAsyncClient client = containerClient.getBlobAsyncClient("brand-" + brand.getId());
+
+        client.upload(BinaryData.fromObject(encryptor.encrypt(brand)), true).subscribe();
+    }
+
+    @Override
+    public void saveSessions(SessionList brand, String id)
+    {
+        BlobAsyncClient client = containerClient.getBlobAsyncClient("sessions-" + id);
+
+        client.upload(BinaryData.fromObject(encryptor.encrypt(brand)), true).subscribe();
+    }
+}
